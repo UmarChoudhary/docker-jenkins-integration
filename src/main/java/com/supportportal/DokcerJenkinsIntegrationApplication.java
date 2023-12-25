8a4ace605035831e574ba54ed0e10e6fef7e9a7a
umarchoudhary <m.umarc57@gmail.com>
2023-12-25 10:28:15 +0530

    first commit

---
+++ src/main/java/com/supportportal/DokcerJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.supportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokcerJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokcerJenkinsIntegrationApplication.class, args);
+	}
+
+}
